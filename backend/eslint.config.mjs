347549f6f0670b3bea69149105ff59dd47894c8a
Никита Спирин <gromilla03@gmail.com>
2026-09-10 23:55:14 +0300

    ci: implement github actions for linting, testing and docker builds

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  {
+    languageOptions: { globals: { ...globals.node, ...globals.jest } }
+  },
+  pluginJs.configs.recommended,
+  {
+    ignores: ["node_modules/", "data/", "backups/"]
+  },
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error"
+    }
+  }
+];
